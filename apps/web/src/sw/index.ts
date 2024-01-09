e71467ce3d875926f92f251f4f1137046c0c4f39
nikomatt69 <nicola.mattioli.95@gmail.com>
2024-01-09 23:36:47 +0100

    10.9.0

--- apps/web/src/sw/index.ts
+++ apps/web/src/sw/index.ts
@@ -1,3 +1,8 @@
+importScripts('https://progressier.app/UyYlhOtlyHyST7enRwK8/sw.js');
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ServiceWorkerCache } from './cache';
+
+
 declare let self: ServiceWorkerGlobalScope;
 
 const impressionsEndpoint = 'https://api.lenshareapp.xyz/leafwatch/impressions';
@@ -26,10 +31,39 @@ const sendVisiblePublicationsToServer = () => {
 
 setInterval(sendVisiblePublicationsToServer, publicationsVisibilityInterval);
 
+const preCachedAssets = (process.env.STATIC_ASSETS ?? []) as string[];
+const CACHEABLE_PATHS = ['/', '/contact', '/explore'];
+const CACHEABLE_DOMAINS = [
+  'https://static-asset.lenshareapp.xyz',
+  'https://asset.lenshareapp.xyz',
+  'https://prerender.lenshareapp.xyz'
+];
+
+const cache = new ServiceWorkerCache({
+  cachePrefix: 'SWCache',
+  cacheableRoutes: [...CACHEABLE_PATHS, ...CACHEABLE_DOMAINS],
+  staticAssets: preCachedAssets
+});
+
+async function handleInstall(): Promise<void> {
+  void self.skipWaiting();
+  await cache.cacheStaticAssets();
+}
+
 const handleActivate = async (): Promise<void> => {
   await self.clients.claim();
+  await cache.invalidatePreviousCache();
 };
 
+const handleFetch = (event: FetchEvent): void => {
+  const request = event.request.clone();
+  const { origin } = new URL(request.url);
+
+  if (self.location.origin === origin || CACHEABLE_DOMAINS.includes(origin)) {
+    event.respondWith(cache.get(event));
+  }
+  return;
+};
 self.addEventListener('message', (event) => {
   // Impression tracking
   if (event.data && event.data.type === 'PUBLICATION_VISIBLE') {
@@ -37,7 +71,8 @@ self.addEventListener('message', (event) => {
     viewerId = event.data.viewerId;
   }
 });
-
+self.addEventListener('fetch', handleFetch);
+self.addEventListener('install', (event) => event.waitUntil(handleInstall()));
 self.addEventListener('activate', (event) => event.waitUntil(handleActivate()));
 
 export {};
