ca718f4985cb47801caa7964eedaed0943265c81
nikomatt69 <nicola.mattioli.95@gmail.com>
2024-02-02 23:07:38 +0100

    15.2

--- packages/ui/src/Image.tsx
+++ packages/ui/src/Image.tsx
@@ -1,4 +1,4 @@
-import { PLACEHOLDER_IMAGE } from '@lensshare/data/constants';
+import { DEFAULT_OG, PLACEHOLDER_IMAGE } from '@lensshare/data/constants';
 import type {
   DetailedHTMLProps,
   ImgHTMLAttributes,
@@ -36,7 +36,7 @@ export const Image = forwardRef(function Image(
   return (
     <img
       {...props}
-      src={imageLoadFailed ? PLACEHOLDER_IMAGE : props.src}
+      src={imageLoadFailed ? DEFAULT_OG : props.src}
       onError={handleError}
       alt={props.alt || ''}
       ref={ref}
