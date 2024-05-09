fd3eed09c2739465f7fc6c480eb3d5522c684da6
Nikoemme.eth <94803063+nikomatt69@users.noreply.github.com>
2024-05-10 01:51:28 +0200

    Update client.ts

--- packages/lens/apollo/client.ts
+++ packages/lens/apollo/client.ts
@@ -6,22 +6,50 @@ import httpLink from './httpLink';
 import retryLink from './retryLink';
 import wsLink from './wsLink';
 
+import { ApolloClient, from, split, ApolloLink, HttpLink } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { RetryLink } from '@apollo/client/link/retry';
+import superfluidLink from './superfluidLink';
+import cache from './cache';
+
+// HTTP and WebSocket links, adjust these imports according to your actual setup
+
+
+// Retry Link configuration
+
+// CSRF prevention link
+const csrfLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      'x-apollo-operation-name': operation.operationName || "UnknownOperation",
+      'apollo-require-preflight': 'true'
+    }
+  }));
+  return forward(operation);
+});
+
+// Combine links for HTTP and WebSocket based on the operation type
 const requestLink = split(
   ({ query }) => {
-    const { kind, operation } = query.definitions[0] as any;
+    const { kind, operation } = query.definitions[0];
     return kind === 'OperationDefinition' && operation === 'subscription';
   },
-  wsLink,
-  httpLink
+  wsLink, // used if the above condition returns true
+  from([csrfLink, httpLink]) // used if the condition returns false, CSRF link included
 );
 
-const apolloClient = (authLink?: ApolloLink) =>
-  new ApolloClient({
+// Function to create Apollo Client instance
+const apolloClient = (authLink?: ApolloLink) => {
+  const link = authLink
+    ? from([authLink, csrfLink, retryLink, requestLink, superfluidLink])
+    : from([csrfLink, retryLink, requestLink, superfluidLink]);
+
+  return new ApolloClient({
     connectToDevTools: true,
-    link: authLink
-      ? from([authLink, retryLink, requestLink, httpLink, superfluidLink])
-      : from([retryLink, httpLink,superfluidLink]),
+    link,
     cache
   });
+};
 
 export default apolloClient;
